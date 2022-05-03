180474cd3f4d07cdc682f34ee5ddc73508aa7feb
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2022-05-02 22:00:24 -0300

    Subida3

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        char letra;
+        char tetra;
         for (int i = 65; i <= 90; i++) {
-            letra = ((char)i);
+            tetra = ((char)i);
 
-            System.out.print(letra + " ");
+            System.out.print(tetra + " ");
         }
 
     }
