87d977d822ba51cb4660870499321e9a1f4338af
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2022-05-02 21:54:21 -0300

    Subida2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        char letra;
+        for (int i = 65; i <= 90; i++) {
+            letra = ((char)i);
+
+            System.out.print(letra + " ");
+        }
+
     }
 }
